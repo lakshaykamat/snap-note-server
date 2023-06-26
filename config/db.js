daee05ce7c5aa8c7deecf7185fb17de7d5d9d649
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-06-27 02:23:56 +0530

    Database connfigured

--- config/db.js
+++ config/db.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
-    console.log(
-      "Database connected: ",
-      connect.connection.host,
-      connect.connection.name
+    console.log({
+      message:"Database connected successfully",
+      database_name:connect.connection.name,
+      host:connect.connection.host,
+      port:connect.connection.port,
+    }
     );
   } catch (err) {
     console.log(err);
